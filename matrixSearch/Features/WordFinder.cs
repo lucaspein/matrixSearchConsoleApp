eaa1f94bbcc033e004572f4327d6fcf134a2e5bf
HIGH PING TEAM <highpingteam@outlook.com>
2021-07-18 01:40:08 -0300

    First horizontal search 1.0

---
+++ matrixSearch/Features/WordFinder.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace matrixSearch.Features
+{
+    public class Counter
+    {
+        public int Times { get; set; }
+        public string Word { get; set; }
+    }
+
+    public class WordFinder
+    {
+        private IEnumerable<string> _matrix;
+
+        private List<Counter> _found;
+
+        public WordFinder(IEnumerable<string> matrix)
+        {
+            this._matrix = matrix;
+            this._found = new List<Counter>();
+        }
+
+        public IEnumerable<string> Find(IEnumerable<string> wordstream)
+        {
+            this._matrix.ToList().ForEach(row => {
+                this.CheckRow(row, wordstream);
+            });
+            
+            var res = from f in _found orderby f.Times descending select f.Word;
+            return res;
+        }
+
+        private void CheckRow(string row, IEnumerable<string> wordstream)
+        {
+            wordstream.ToList().ForEach(word => { 
+                if(row.Contains(word))
+                {
+                    WordFound(word);
+                }
+            });
+        }
+
+        private void WordFound(string word)
+        {
+            var alreadyAdded = _found.Where(w => w.Word.ToLower() == word.ToLower()).FirstOrDefault();
+            if (alreadyAdded != null)
+            {
+                alreadyAdded.Times += 1;
+            }
+            else
+            {                
+                _found.Add(new Counter() { Word = word, Times = 1 });
+            }
+        }
+    }
+}
---
+++ matrixSearch/Program.cs
@@ -0,0 +1,38 @@
+﻿using matrixSearch.Features;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace matrixSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var matrix = Enumerable.Empty<string>();
+            var words = Enumerable.Empty<string>();
+            var found = Enumerable.Empty<string>();
+
+            if (args.Length > 0)
+            {
+                Console.WriteLine("with args!");
+                matrix = args[0].Split(',');
+                words = args[1].Split(',');
+            }
+            else
+            {
+                matrix = new string[] { "chill", "fgwio", "chill", "pqnsd", "uvdxy" };
+                words = new string[] { "chill", "fgwio", "wind" };
+            }           
+
+            var finder = new WordFinder(matrix);
+            found = finder.Find(words).AsEnumerable<string>();
+            Console.Write("Words found order by times: ");
+            Console.WriteLine(found);
+            Console.ReadLine();
+        }
+
+
+    }
+}
